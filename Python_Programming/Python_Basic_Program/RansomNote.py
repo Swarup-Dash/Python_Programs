e321ea2794221a8071b1169c86516784d097f29b
Swarup-Dash <swarupdash7205@gmail.com>
2024-05-24 08:25:48 +0530

    RansomNote

--- Python_Programming/Python_Basic_Program/RansomNote.py
+++ Python_Programming/Python_Basic_Program/RansomNote.py
@@ -1,13 +1,25 @@
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         rans=''.join(sorted(ransomNote))
+#         mang=''.join(sorted(magazine))
+#         # print(rans)
+#         # print(mang)
+#         if rans in mang:
+#             return True
+#         else:
+#             return False 
+
+from collections import Counter
+       
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        rans=''.join(sorted(ransomNote))
-        mang=''.join(sorted(magazine))
-        # print(rans)
-        # print(mang)
-        if rans in mang:
-            return True
-        else:
-            return False 
+        ransom_count = Counter(ransomNote)
+        magazine_count = Counter(magazine)
+        
+        for char, count in ransom_count.items():
+            if magazine_count[char] < count:
+                return False
+        return True
 obj=Solution()
 ransomNote = "aab"
 magazine = "baa"
