d396ea172d4bcfec2fce1ab188811f6a904ad143
Swarup-Dash <swarupdash7205@gmail.com>
2024-05-25 09:17:08 +0530

    Word_pattern Program

---
+++ Python_Programming/Python_Basic_Program/word_pattern.py
@@ -0,0 +1,28 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        if len(pattern) != len(words):
+            return False
+        
+        char_to_word = {}
+        word_to_char = {}
+        
+        for char, word in zip(pattern, words):
+            if char in char_to_word:
+                if char_to_word[char] != word:
+                    return False
+            else:
+                char_to_word[char] = word
+            
+            if word in word_to_char:
+                if word_to_char[word] != char:
+                    return False
+            else:
+                word_to_char[word] = char
+        
+        return True
+obj=Solution()
+pattern = "abba"
+s = "dog cat cat dog"
+ans=obj.wordPattern(pattern, s)
+print(ans)
