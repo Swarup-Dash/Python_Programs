bc8d4c1c8f0b34a0857897dd4f99523d2542ebdd
Swarup-Dash <swarupdash7205@gmail.com>
2024-05-26 23:32:04 +0530

    try one

---
+++ Python_Programming/Python_Basic_Program/Valid_anagram1.py
@@ -0,0 +1,11 @@
+#For learning Purpose
+s = "eat"
+t = "tea"
+s1=sorted(s)
+print(s1)
+t1=sorted(t)
+print(t1)
+if s1==t1:
+    print("It is a Valid Anagram..")
+else:
+    print("Not Valid Anagtam..")
