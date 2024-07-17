806a79d114995bbcf83eb38a2376259d0ba01b8a
Swarup-Dash <swarupdash7205@gmail.com>
2024-07-17 22:36:35 +0530

    initial Commit

---
+++ Python_Programming/Python_Basic_Program/CountItems.py
@@ -0,0 +1,16 @@
+def charCount(lst):
+    counter = {}
+    for i in lst:
+        if i in counter:
+            counter[i]+=1
+        else:
+            counter[i] = 1
+            
+    return counter
+    
+    
+    
+    
+lst = ['a', 'b', 'a', 'c', 'a', 'b']
+res = charCount(lst)
+print(res)
